56e38b57fb8ec5560d6eddd79ecb9209228909c4
Neyasbltb <neyasbltb_88@mail.ru>
2019-07-13 23:24:11 +0300

    Работа с массвом тасков

--- app/scripts/common.js
+++ app/scripts/common.js
@@ -1,11 +1,28 @@
-window.launch = new Launcher({
+let tasks = [{
     condition: () => window.test === 5,
     callback: function(text) {
         console.log(text);
 
-        window.test = 0;
-        this.run();
+        // window.test = 0;
+        // this.run();
     },
     arg: () => 'window.test равен ' + window.test,
+    name: 'test5',
+    // attempts: 50,
+    // run: true
+}, {
+    condition: () => window.test === 4,
+    callback: function(text) {
+        console.log(text);
+
+        // window.test = 0;
+        // this.run();
+    },
+    arg: () => 'window.test равен ' + window.test,
+    name: 'test4',
+    // attempts: 50,
     run: true
-});
+}];
+
+window.launch = new Launcher(tasks);
+// window.launch = new Launcher();
--- app/scripts/task-launcher.js
+++ app/scripts/task-launcher.js
@@ -1,65 +1,192 @@
 export default class Launcher {
     running = false;
-    wasStopped = false;
-    tryNum = 0;
+    rafId = 0;
+    tasks = [];
 
-    /*  props = {
+    /*  tasks = {
      *       condition:  @Function: должна проверять условие, необходимое для вызова callback и возвращать boolean.
      *       callback:   @Function: вызовется когда condition вернет true.
+     *       name:       @String: Имя таска для более удобного управления им. Необязательный.
      *       arg:        @Any: один аргумент, который передастся в callback при его вызове. Необязательный.
      *                   @Function: если передана функция, то все, что она вернет, будет передано в callback при его вызове.
-     *       attempts:   @Number: количество попыток для проверки условия. Необязательный. 
-     *                   Если не пердан, проверка будет бесконечной, пока condition не вернет true или не будет вызван метод остановки .stop()
-     *       run:        @Boolean: если true, то запуск таска произойдет автоматически, без вызова .run()
+     *       attempts:   @Number: количество попыток для проверки условия. Необязательный.
+     *                   Если не передан, проверка будет бесконечной, пока condition не вернет true или не будет вызван метод остановки .stop()
+     *       run:        @Boolean: если true, то запуск таска произойдет автоматически, без вызова .run(). Необязательный.
      *   } */
-    constructor(props = {}) {
-        this.condition = props.condition || true;
-        this.callback = props.callback || console.log.bind(this, 'В Laucher не передан callback');
-        this.arg = props.arg;
-        this.attempts = props.attempts || Infinity;
-
-        if (props.run) this.run();
+    constructor(tasks = []) {
+        this.add(tasks);
     }
 
-    launch = () => {
-        // Если функция условия вернула true, тормозим рекурсию и выззываем callback
-        if (this.condition.call(this)) {
-            this.stop();
-            this.callback.call(this, typeof this.arg === 'function' ? this.arg.call(this) : this.arg);
+    add = (tasks) => {
+        let newTasks = [];
+        if (tasks instanceof Array) {
+            newTasks = tasks;
+        } else if (tasks instanceof Object) {
+            newTasks.push(tasks);
+        }
 
-            // Если функция условия вернула false, количество попыток не исчерпано и не остановлена рекурсия,
-            // то увеличиваем счетчик попыток и планируем очередной запуск 
-        } else if (!this.condition.call(this) && this.tryNum < this.attempts && !this.wasStopped) {
-            this.tryNum++;
-            requestAnimationFrame(this.launch);
+        newTasks.forEach(task => {
+            // Валидация таска
+            if (!task.condition || typeof task.condition !== 'function') {
+                console.warn('Laucher: не передано условие', task);
+                return;
+            }
+            if (!task.callback || typeof task.callback !== 'function') {
+                console.warn('Laucher: не передан коллбек', task);
+                return;
+            }
 
-            // Если количество попыток исчерпано, тормозим рекурсию
-        } else if (this.tryNum >= this.attempts) {
+            // Подготовка таска
+            task.attempts = tasks.attempts || Infinity;
+            task.tryNum = 0;
+            task.run = task.run || false;
+
+            // Добавление в общий массив тасков
+            this.tasks.push(task);
+        });
+
+        // Попытка запуска тасков после добавления
+        this.rafId = requestAnimationFrame(this._launch);
+    }
+
+    hasActive = () => {
+        return this.tasks.some(task => task.run);
+    }
+
+    _launch = (rafId) => {
+        // Если не рекурсивный запуск и уже запущен процесс проверки тасков, или нет активных, то выходим
+        if (this.rafId !== rafId && (this.running || !this.hasActive())) return;
+        this.running = true;
+
+        this.tasks.forEach((task, index) => {
+            // Если таск не активный, то выходим
+            if (!task.run) return;
+
+            // Если функция условия таска вернула true, отключаем дальнейшее выполнение таска и выззываем callback
+            if (task.condition.call(this)) {
+                this.stop(index);
+
+                task.callback.call(this, typeof task.arg === 'function' ? task.arg.call(this) : task.arg);
+
+                // Если функция условия вернула false и количество попыток исчерпано, тормозим таск
+            } else if (!task.condition.call(this)) {
+                task.tryNum++;
+
+                if (task.tryNum >= task.attempts) {
+                    this.stop(index);
+                }
+
+            }
+        });
+
+        // После цикла по таскам проверяем остались ли активные таски
+
+        // Если активные таски есть, планируем следующий запуск _launch
+        if (this.hasActive()) {
+            this.rafId = requestAnimationFrame(() => this._launch(this.rafId));
+
+            // Если таски все завершены, то переходим в глобальное состояние остановки
+        } else {
             this.stop();
         }
+
     }
 
-    // Запуск рекурсивной проверки условия
-    run = (arg) => {
-        if (this.running) return;
-        if (arg !== undefined) this.arg = arg;
+    _findTask(taskId) {
+        let index;
+        // Если число - то это индекс, и если под индексом есть таск
+        if (typeof taskId === 'number' && this.tasks[taskId]) {
+            index = taskId;
 
-        this.running = true;
-        this.wasStopped = false;
-        this.launch();
+            // Если строка, то это имя
+        } else if (typeof taskId === 'string') {
+            this.tasks.forEach((task, ind) => {
+                if (task.name === taskId) index = ind;
+            });
+        } else {
+            return false;
+        }
 
-        return true;
+        return index;
+    }
+
+    _runTask = (taskId, arg) => {
+        let index = this._findTask(taskId);
+        if (index === false) return false;
+
+        // Если нет аргумениа и таск уже запущен, возвращаем false
+        if (!arg && this.tasks[index].run) return false;
+
+        // Запускаем таск
+        this.tasks[index].run = true;
+        // Если есть аргумент, обновим его в таске
+        if (arg) this.tasks[index].arg = arg;
+
+        return index;
+    }
+
+    // Запуск рекурсивной проверки условий тасков
+    run = (...args) => {
+        let runs;
+        // Если аргументов нет, то запуск всех тасков
+        if (args.length === 0) {
+            runs = [];
+
+            this.tasks.forEach((task, index) => {
+                if (task.run === false) {
+                    runs.push(index);
+                    task.run = true;
+                }
+            });
+
+            // Если аргумент один, то это индекс или имя таска
+        } else if (args.length === 1) {
+            runs = this._runTask(args[0]);
+
+            // Если аргумента два, то в первом индекс/имя таска, а во втором обновление для task.arg
+        } else if (args.length === 2) {
+            runs = this._runTask(args[0], args[1]);
+        }
+
+        // Запуск выполнения проверки активных тасков
+        this.rafId = requestAnimationFrame(this._launch);
+
+        return runs;
+    }
+
+    _stopTask(taskId) {
+        let index = this._findTask(taskId);
+        if (index === false) return false;
+
+        this.tasks[index].run = false;
+        this.tasks[index].tryNum = 0;
+
+        return index;
     }
 
     // Остановка рекурсивной проверки условия
-    stop = () => {
-        if (!this.running) return;
+    stop = (taskId) => {
+        let stoped;
+        if (taskId !== undefined) {
+            let index = this._stopTask(taskId);
+            if (index === false) return false;
 
-        this.wasStopped = true;
-        this.running = false;
-        this.tryNum = 0;
+            stoped = index;
+        } else {
+            stoped = [];
+            this.tasks.forEach((task, idx) => {
+                let index = this._stopTask(idx);
+                if (index === false) return false;
 
-        return true;
+                stoped.push(index);
+            });
+
+            this.running = false;
+            cancelAnimationFrame(this.rafId);
+            this.rafId = 0;
+        }
+
+        return stoped;
     }
 }
 
